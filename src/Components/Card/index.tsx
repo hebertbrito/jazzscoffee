df68f56cff7662f8a08da5133aa84a7544c75e40
hebert felipe <hebertfelipe.97@outlook.com.br>
2020-07-18 20:58:55 -0300

    added changes

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -12,10 +12,9 @@ export function Card(props: CardUI) {
             <div>
                 <h2 className="title_info">Today's news</h2>
                 <div className="carddois">
-                    <h5 className="safra">Safra de café 20/21</h5>
-                    <p className="texto">De acordo com o Cecafé, com relação à demanda global,
-                    há poucos dados precisos sobre o comportamento do consumidor,
-                    principalmente durante a pandemia...15/07/2020</p>
+                    <h5 className="safra">Coffee harvest 20/21</h5>
+                    <p className="texto">According to Cecafé, with regard to global demand, there is little accurate data on consumer behavior, 
+                    especially during the pandemic....07/15/2020</p>
                 </div>
             </div>
         </div>
