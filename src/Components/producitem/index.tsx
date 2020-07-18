f57989d8bf7a5b9b14600b0b5c750ea6598b15a4
hebert felipe <hebertfelipe.97@outlook.com.br>
2020-07-18 20:42:21 -0300

    added changes css

--- src/Components/producitem/index.tsx
+++ src/Components/producitem/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import './styles.css'
-import image2 from '../../Assets/irishcoffee.jpg'
 
-export function ProductItem() {
+interface ProductitemUI {
+    img?: JSX.Element
+    nameProduct: string
+    tipoProduct: string
+}
 
-    const img2 = <img src={image2} className="imgproduct" />
+export function ProductItem(props: ProductitemUI) {
 
     return (
         <div className="container_product_item">
             <div className="products">
                 <div className="product_item">
-                    {img2}
+                    {props.img}
                 </div>
             </div>
             <div className="product_info">
-                <p className="description_item"><p className="ind_product">Produto: </p>Cappucino</p>
-                <p className="description_item"><p className="ind_product">Tipo: </p>Alcolico</p>
+                <p className="description_item"><p className="ind_product">Produto: </p>{props.nameProduct}</p>
+                <p className="description_item"><p className="ind_product">Tipo: </p>{props.tipoProduct}</p>
             </div>
         </div>
     );
--- src/Pages/Product/index.tsx
+++ src/Pages/Product/index.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import { Divider } from '@material-ui/core';
 import './styles.css'
+import image2 from '../../Assets/irishcoffee.jpg'
 
 import { ProductItem } from '../../Components/producitem'
+import { url } from 'inspector';
+
+const lstProduct = [] = [
+    {
+        img: <img src={require('../../Assets/irishcoffee.jpg')} className="imgproduct" />,
+        nameProduct: 'Irish Coffee',
+        typeProduct: 'Alcoholic'
+    },
+    {
+        img: <img src={require('../../Assets/coldbrew.jpg')} className="imgproduct" />,
+        nameProduct: 'Cold Brew',
+        typeProduct: 'Ice Cold / Smooth'
+    },
+    {
+        img: <img src={require('../../Assets/cafegelado.jpg')} className="imgproduct" />,
+        nameProduct: 'Iced Coffee',
+        typeProduct: 'Ice Cold'
+    },
+    {
+        img: <img src={require('../../Assets/cafeafogatto.jpg')} className="imgproduct" />,
+        nameProduct: 'Affogato Coffe',
+        typeProduct: 'Dessert / Iced Cold'
+    },
+    {
+        img: <img src={require('../../Assets/donuts.jpg')} className="imgproduct" />,
+        nameProduct: 'Doughnut',
+        typeProduct: 'Dessert'
+    },
+]
 
 class Products extends React.Component {
     render() {
@@ -10,17 +40,14 @@ class Products extends React.Component {
             <div className="containerproduct">
                 <div className="cont_title">
                     <div className="divider"></div>
-                    {/* <Divider style={{ backgroundColor: '#fafafa', height: '3px', width: '15%', alignSelf: 'center' }} /> */}
                     <p className="title_prod">News products</p>
-                    {/* <Divider style={{ backgroundColor: '#fafafa', height: '2px', width: '15%', alignSelf: 'center' }} /> */}
                     <div className="divider"></div>
 
                 </div>
                 <div className="produc_subs">
-                    <ProductItem />
-                    <ProductItem />
-                    <ProductItem />
-                    <ProductItem />
+                    {lstProduct.map(x => (
+                        <ProductItem img={x.img} nameProduct={x.nameProduct} tipoProduct={x.typeProduct} />
+                    ))}
                 </div>
             </div>
         );
